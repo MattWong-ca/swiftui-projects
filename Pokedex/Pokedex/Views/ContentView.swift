8d8d0d068d3141295146e7abd593eeb6c1b0df9e
MattWong-ca <matthew.yt.wong@gmail.com>
2023-07-24 16:02:24 -0400

    Add search bar functionality

--- Pokedex/Pokedex/Views/ContentView.swift
+++ Pokedex/Pokedex/Views/ContentView.swift
@@ -6,55 +6,16 @@
 //
 
 import SwiftUI
-/*
-struct ContentView: View {
-    var pokedex: [Pokemon]
-    @State private var searchText = ""
-    
-    var body: some View {
-        NavigationView {
-            List(pokedex) { pokemon in
-                NavigationLink(destination: PokemonDetailView(pokemon: pokemon)) {
-                    HStack {
-                        Text("#" + String(format: "%03d", pokemon.id))
-                            .font(.subheadline)
-                        Text(pokemon.name)
-                            .font(.headline)
-                        
-                        Spacer()
-                        
-                        PokemonTypeView(pokemonType: pokemon.type1)
-                        
-                        if !pokemon.type2.isEmpty {
-                            PokemonTypeView(pokemonType: pokemon.type2)
-                        }
-                    }
-                }
-            }
-            .navigationTitle("Pokédex")
-            .searchable(text: $searchText) {
-                ForEach(filteredPokemon, id: \.self) { pokemon in
-                    Text(pokemon)
-                }
-            }
-        }
-    }
-    
-    private var filteredPokemon: [String] {
-        let arrayOfPokemonNames: [String] = pokedex.map { pokemon in
-            return pokemon.name
-        }
-        
-        return arrayOfPokemonNames.filter {
-            $0.localizedCaseInsensitiveContains(searchText)
-        }
-    }
-}
-*/
 
 struct ContentView: View {
     var pokedex: [Pokemon]
     @State private var searchText = ""
+    
+    private var filteredPokemon: [Pokemon] {
+        return pokedex.filter { pokemon in
+            searchText.isEmpty || pokemon.name.localizedCaseInsensitiveContains(searchText)
+        }
+    }
     
     var body: some View {
         NavigationStack {
@@ -80,25 +41,17 @@ struct ContentView: View {
             }
             .navigationTitle("Pokédex")
             .searchable(text: $searchText) {
+                // When searching, if we want to keep the same list and
+                // not have a new list with just the Pokemon names, we
+                // can delete the ForEach block and use filteredPokemon
+                // on line 23 instead
                 ForEach(filteredPokemon) { pokemon in
                     NavigationLink(destination: PokemonDetailView(pokemon: pokemon)) {
-                        HStack {
-//                            Text("#" + String(format: "%03d", pokemon.id))
-//                                .font(.subheadline)
-                            Text(pokemon.name)
-//                                .font(.headline)
-                                .foregroundColor(.black)
-                        }
+                        Text(pokemon.name)
+                            .foregroundColor(.black)
                     }
-                    
                 }
             }
-        }
-    }
-    
-    private var filteredPokemon: [Pokemon] {
-        return pokedex.filter { pokemon in
-            searchText.isEmpty || pokemon.name.localizedCaseInsensitiveContains(searchText)
         }
     }
 }
