c16b2fd76958347678531e4bd84adf6fd605580c
MattWong-ca <matthew.yt.wong@gmail.com>
2023-07-23 22:56:55 -0400

    Add search bar

--- Pokedex/Pokedex/Views/ContentView.swift
+++ Pokedex/Pokedex/Views/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var pokedex: [Pokemon]
+    @State private var searchText = ""
     
     var body: some View {
         NavigationView {
@@ -31,8 +32,38 @@ struct ContentView: View {
                 }
             }
             .navigationTitle("Pokédex")
+            .searchable(text: $searchText) {
+//                ForEach(filteredPokemon) { pokemon in
+//                    Text($searchText)
+//                }
+            }
         }
     }
+    
+    private var filteredPokemon: [String] {
+//        let arrayOfPokemonNames: [String]
+//
+//        ForEach(pokedex) { pokemon in
+//            arrayOfPokemonNames.append(pokemon.name)
+//        }
+        
+        let arrayOfPokemonNames: [String] = pokedex.map { pokemon in
+            return pokemon.name
+        }
+        
+        return arrayOfPokemonNames.filter {
+            $0.localizedCaseInsensitiveContains(searchText)
+        }
+    }
+    
+    /*private var filteredItems: [String] {
+            // Filter the list items based on the searchText
+            // You'll have to implement this filtering logic based on your data
+            // For this example, I'm just returning a static list
+            let items = ["Item 1", "Item 2", "Item 3", "Another Item"]
+            return items.filter { $0.localizedCaseInsensitiveContains(searchText) }
+        }*/
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
