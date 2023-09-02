7462ae0472e509b8f66684a3e81f5a44bd7c83f0
MattWong-ca <matthew.yt.wong@gmail.com>
2023-09-02 19:12:40 -0400

    Add better sweater photos

--- SweaterShop/SweaterShop/Models/Product.swift
+++ SweaterShop/SweaterShop/Models/Product.swift
@@ -14,8 +14,11 @@ struct Product: Identifiable {
     var price: Int
 }
 
-var productList = [ Product(name: "Zesty Orange", image: "sweater1", price: 59),
-                    Product(name: "Summer", image: "sweater2", price: 59),
-                    Product(name: "Winter", image: "sweater3", price: 59),
-                    Product(name: "Maroon", image: "sweater4", price: 59),
-                    Product(name: "Green", image: "sweater5", price: 59)]
+var productList = [ Product(name: "Zesty Orange", image: "sweater1", price: 39),
+                    Product(name: "Ivory Intrigue", image: "sweater4", price: 39),
+                    Product(name: "Beige Bliss", image: "sweater3", price: 29),
+                    Product(name: "Mossy Magic", image: "sweater2", price: 39),
+                    Product(name: "Fairy Floss", image: "sweater5", price: 39),
+                    Product(name: "Silver Lining", image: "sweater6", price: 39),
+                    Product(name: "Maroon Marvel", image: "sweater7", price: 29),
+                    Product(name: "Lavender Skies", image: "sweater8", price: 39)]
