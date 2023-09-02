6bc082ae6af51691e876cbd532a7d3692306be82
MattWong-ca <matthew.yt.wong@gmail.com>
2023-09-01 20:13:58 -0400

    Add cart button

---
+++ SweaterShop/SweaterShop/Components/CartButton.swift
@@ -0,0 +1,34 @@
+//
+//  CartButton.swift
+//  SweaterShop
+//
+//  Created by Matthew Wong on 2023-09-01.
+//
+
+import SwiftUI
+
+struct CartButton: View {
+    var numberOfProducts: Int
+    
+    var body: some View {
+        ZStack(alignment: .topTrailing) {
+            Image(systemName: "cart")
+                .padding(.top, 5)
+            
+            if numberOfProducts > 0 {
+                Text("\(numberOfProducts)")
+                    .font(.caption).bold()
+                    .foregroundColor(.white)
+                    .frame(width: 15, height: 15)
+                    .background(Color(hue: 1.0, saturation: 0.986, brightness: 0.963))
+                    .cornerRadius(50)
+            }
+        }
+    }
+}
+
+struct CartButton_Previews: PreviewProvider {
+    static var previews: some View {
+        CartButton(numberOfProducts: 1)
+    }
+}
--- SweaterShop/SweaterShop/Views/ContentView.swift
+++ SweaterShop/SweaterShop/Views/ContentView.swift
@@ -11,13 +11,19 @@ struct ContentView: View {
     var columns = [GridItem(.adaptive(minimum: 160), spacing: 20)]
     
     var body: some View {
-        ScrollView {
-            LazyVGrid(columns: columns, spacing: 20) {
-                ForEach(productList, id: \.id) { product in
-                    ProductCard(product: product)
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: columns, spacing: 20) {
+                    ForEach(productList, id: \.id) { product in
+                        ProductCard(product: product)
+                    }
                 }
+                .padding()
             }
-            .padding()
+            .navigationTitle(Text("Sweater Shop"))
+            .toolbar {
+                CartButton(numberOfProducts: 1)
+            }
         }
     }
 }
