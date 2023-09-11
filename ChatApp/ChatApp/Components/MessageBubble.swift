ce8acd8f5c91eed625bcdf511c7fcc830a015c34
MattWong-ca <matthew.yt.wong@gmail.com>
2023-09-10 22:45:48 -0400

    Update message bubble UI

--- ChatApp/ChatApp/Components/MessageBubble.swift
+++ ChatApp/ChatApp/Components/MessageBubble.swift
@@ -9,17 +9,33 @@ import SwiftUI
 
 struct MessageBubble: View {
     var message: Message
+    @State private var showTime = false
+    
     var body: some View {
         VStack(alignment: message.received ? .leading : .trailing) {
             HStack {
                 Text(message.text)
+                    .padding()
+                    .background(message.received ? Color("Gray") : Color("Peach"))
+                    .cornerRadius(30)
+            }
+            .frame(maxWidth: 300, alignment: message.received ? .leading : .trailing)
+            .onTapGesture {
+                showTime.toggle()
+            }
+            
+            if showTime {
+                
             }
         }
+        .frame(maxWidth: .infinity, alignment: message.received ? .leading : .trailing)
+        .padding(message.received ? .leading : .trailing)
+        .padding(.horizontal, 10)
     }
 }
 
 struct MessageBubble_Previews: PreviewProvider {
     static var previews: some View {
-        MessageBubble(message: Message(id: "1", text: "Hi", received: false, timestamp: Date()))
+        MessageBubble(message: Message(id: "1", text: "Hi it's me, this is a test message", received: false, timestamp: Date()))
     }
 }
