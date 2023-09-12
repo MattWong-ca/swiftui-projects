4af6f877e05af631973685ca0d441c07ed805bd2
MattWong-ca <matthew.yt.wong@gmail.com>
2023-09-12 18:38:20 -0400

    Add temporary scroll view to show messages

--- ChatApp/ChatApp/ContentView.swift
+++ ChatApp/ChatApp/ContentView.swift
@@ -8,10 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    // Temporary
+    var messageArray = ["Hello", "How ya doing", "I'm great!"]
+    
     var body: some View {
         VStack {
             TitleRow()
                 .background(Color("Peach"))
+            
+            ScrollView {
+                ForEach(messageArray, id: \.self) { text in
+                    MessageBubble(message: Message(id: "123", text: text, received: true, timestamp: Date()))
+                }
+            }
         }
     }
 }
