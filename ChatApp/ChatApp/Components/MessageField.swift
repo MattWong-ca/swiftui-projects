8f417dc241b665f6d54d87f8f330d1f5c36ae58a
MattWong-ca <matthew.yt.wong@gmail.com>
2023-10-12 23:56:38 -0400

    Update message field UI

--- ChatApp/ChatApp/Components/MessageField.swift
+++ ChatApp/ChatApp/Components/MessageField.swift
@@ -10,7 +10,19 @@ import SwiftUI
 struct MessageField: View {
     @State private var message = ""
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            CustomTextField(placeholder: Text("Enter your message here"), text: $message)
+            
+            Button {
+                print("message sent!")
+            } label: {
+                Image(systemName: "paperplane.fill")
+                    .foregroundColor(.white)
+                    .padding(10)
+                    .background(Color("Peach"))
+                    .cornerRadius(50)
+            }
+        }
     }
 }
 
